35823041687bddbb38fa6c170b781cbc8341a8aa
paulo <paulo@suaid.io>
2023-09-04 16:54:21 -0300

    feat: creating species enum

---
+++ src/main/java/com/github/paulovalleriote/dugeonexplorer/domain/models/sheet/SpecieEnum.java
@@ -0,0 +1,23 @@
+package com.github.paulovalleriote.dugeonexplorer.domain.models.sheet;
+
+public enum SpecieEnum {
+  DRAGONBORN("dragonborn"),
+  DWARF("dwarf"),
+  ELF("elf"),
+  GNOME("gnome"),
+  HALF_ELF("half elf"),
+  HALFLING("halfling"),
+  HALF_ORC("half orc"),
+  HUMAN("human"),
+  TIEFLING("tiefling");
+
+  private String specie;
+
+  SpecieEnum(String characterClass) {
+    this.specie = characterClass;
+  }
+
+  public String getSpecie() {
+    return specie;
+  }
+}
