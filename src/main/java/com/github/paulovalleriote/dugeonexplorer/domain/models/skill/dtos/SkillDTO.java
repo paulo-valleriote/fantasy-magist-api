0bea1002dc2551b4f28d2f180012771c367e1d44
paulo <paulo@suaid.io>
2023-09-04 15:57:57 -0300

    feat: creating default Skill DTO

---
+++ src/main/java/com/github/paulovalleriote/dugeonexplorer/domain/models/skill/dtos/SkillDTO.java
@@ -0,0 +1,10 @@
+package com.github.paulovalleriote.dugeonexplorer.domain.models.skill.dtos;
+
+import lombok.Data;
+
+@Data
+public class SkillDTO {
+  private String name;
+  private String description;
+
+}
