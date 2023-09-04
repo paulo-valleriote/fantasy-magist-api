145e213cb98474f025e32f9a24a21e3ede5bedb7
paulo <paulo@suaid.io>
2023-09-04 16:02:41 -0300

    feat: implementating Item create method

---
+++ src/main/java/com/github/paulovalleriote/dugeonexplorer/services/ItemService.java
@@ -0,0 +1,23 @@
+package com.github.paulovalleriote.dugeonexplorer.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.github.paulovalleriote.dugeonexplorer.domain.models.bag.Bag;
+import com.github.paulovalleriote.dugeonexplorer.domain.models.item.Item;
+import com.github.paulovalleriote.dugeonexplorer.domain.models.item.dtos.ItemDTO;
+import com.github.paulovalleriote.dugeonexplorer.mapper.Mapper;
+import com.github.paulovalleriote.dugeonexplorer.repositories.ItemRepository;
+
+@Service
+public class ItemService {
+  @Autowired
+  private ItemRepository repository;
+
+  public ItemDTO create(ItemDTO spell, Bag bag) {
+    Item newItem = Mapper.parseObject(spell, Item.class);
+    newItem.setBag(bag);
+
+    return Mapper.parseObject(repository.save(newItem), ItemDTO.class);
+  }
+}
