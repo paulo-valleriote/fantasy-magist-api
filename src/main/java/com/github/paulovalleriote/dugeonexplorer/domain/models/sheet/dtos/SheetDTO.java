e2785f18f25eff6b959ec0e0c08e5ef893e81c52
paulo <paulo@suaid.io>
2023-11-23 13:32:31 -0300

    adding custom constructor

--- src/main/java/com/github/paulovalleriote/dugeonexplorer/domain/models/sheet/dtos/SheetDTO.java
+++ src/main/java/com/github/paulovalleriote/dugeonexplorer/domain/models/sheet/dtos/SheetDTO.java
@@ -3,6 +3,7 @@ package com.github.paulovalleriote.dugeonexplorer.domain.models.sheet.dtos;
 import com.github.paulovalleriote.dugeonexplorer.domain.models.sheet.ClassEnum;
 import com.github.paulovalleriote.dugeonexplorer.domain.models.sheet.SpecieEnum;
 
+import com.github.paulovalleriote.dugeonexplorer.services.SheetService;
 import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -17,4 +18,12 @@ public class SheetDTO {
   private int armorClass;
   private int proficiency;
   private int level;
+  public SheetDTO(String name, SpecieEnum specie, ClassEnum characterClass, int level) {
+    this.name = name;
+    this.specie = specie;
+    this.characterClass = characterClass;
+    this.level = level;
+    this.armorClass = 12;
+    this.proficiency = SheetService.calculateProficiency(level);
+  }
 }
